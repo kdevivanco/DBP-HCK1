dc005194320d498c07cb2bee59d4a39bb7dd0afc
Kayla de Vivanco <kayla.devivanco@utec.edu.pe>
2023-10-02 16:15:40 -0500

    project setup

---
+++ src/main/java/com/example/hck1/Hck1Application.java
@@ -0,0 +1,13 @@
+package com.example.hck1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hck1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hck1Application.class, args);
+    }
+
+}
